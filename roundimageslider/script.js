5aba979ebc46f90460f145c042fc26f8da1986a8
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-24 08:55:24 +0530

    Round image slider  project  added successfully

---
+++ roundimageslider/script.js
@@ -0,0 +1,26 @@
+const imageSliderEl=document.querySelector(".image-slider");
+const prevEl=document.getElementById("prev");
+const nextEl=document.getElementById("next");
+let x=0;
+let timer;
+prevEl.addEventListener("click",()=>
+{
+x=x+45;
+clearTimeout(timer);
+updateGallery();
+});
+nextEl.addEventListener("click",()=>
+    {
+    x=x-45;
+    clearTimeout(timer);
+    updateGallery();
+    });
+function updateGallery(){
+    imageSliderEl.style.transform=`perspective(1000px) rotateY(${x}deg)`;
+    timer=setTimeout(()=>{
+      x=x-45;
+      updateGallery()
+    },2000);
+
+}
+updateGallery();
